42e7f7e850f347b67452d26fc358700acf33cbde
jackiedjq <jiaqid375@gmail.com>
2025-06-18 14:53:39 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_bHx95HLMQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '38fq9kdv120iql7n3h66989kar', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_DGHSl9bqv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'r1imdkknn8ca0oobk19g6emm2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
